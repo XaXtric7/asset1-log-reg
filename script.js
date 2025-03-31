9b004e21b476f686e9a365699ae19e8320f698d1
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-03-31 07:23:53 +0530

    fixed_few_bugs

--- script.js
+++ script.js
@@ -19,3 +19,20 @@ btnPopup.addEventListener("click", () => {
 iconClose.addEventListener("click", () => {
   wrapper.classList.remove("active-popup");
 });
+
+// Add event listeners for input fields to ensure labels stay up when text is entered
+document.querySelectorAll(".input-box input").forEach((input) => {
+  // Check if input already has value on page load
+  if (input.value) {
+    input.classList.add("has-value");
+  }
+
+  // Add input event listener to track changes
+  input.addEventListener("input", function () {
+    if (this.value) {
+      this.classList.add("has-value");
+    } else {
+      this.classList.remove("has-value");
+    }
+  });
+});
